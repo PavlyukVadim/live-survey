344b5c07600a339f0404b5ca0f4079c3949a57cf
PavlyukVadim <pavlyuk.dev@gmail.com>
2019-05-21 23:50:46 +0300

    output form

--- client/src/modules/form-filler/index.js
+++ client/src/modules/form-filler/index.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
+import LiveForm from 'react-live-form'
+import { connect } from 'react-redux'
+import { getCurrentForm } from '../../actions/requestActions'
 
 class FormFiller extends Component {
-  render() {
-    const { match = {} } = this.props
+  componentDidMount() {
+    const { getForm, match = {} } = this.props
     const { id } = match.params
+    if (getForm) {
+      getForm(id)
+    }
+  }
+
+  render() {
+    const {
+      match = {},
+      preloadedForm,
+    } = this.props
+    const { id } = match.params
+    const formConfig = {}
 
     console.log('id', id)
 
     return (
       <div>
         FormFiller
+        {
+          preloadedForm && (
+            <LiveForm formConfig={preloadedForm.formConfig} />
+          )
+        }
       </div>
     )
   }
 }
 
-export default withRouter(FormFiller)
+const mapStateToProps = (state) => ({
+  preloadedForm: state.forms && state.forms.currentForm,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  getForm: (id) => dispatch(getCurrentForm(id)),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withRouter(FormFiller))
