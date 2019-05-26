49507a8204cc503a68a97f57e1b25d774a119e32
PavlyukVadim <pavlyuk.dev@gmail.com>
2019-05-26 11:07:30 +0300

    update libraries

--- client/src/modules/form-filler/index.js
+++ client/src/modules/form-filler/index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import LiveForm from 'react-live-form'
 import { connect } from 'react-redux'
+import { Header } from 'semantic-ui-react'
 import { getCurrentForm } from '../../actions/requestActions'
+import './index.scss'
 
 class FormFiller extends Component {
   componentDidMount() {
@@ -21,11 +23,17 @@ class FormFiller extends Component {
     const { id } = match.params
     const formConfig = {}
 
+    const { title, description } = (preloadedForm || {})
+
     console.log('id', id)
+    console.log('preloadedForm', preloadedForm)
 
     return (
-      <div>
-        FormFiller
+      <div className="form-filler">
+        <div className="header">
+          <Header as='h2'>{title}</Header>
+          <Header as='h3'>{description}</Header>
+        </div>
         {
           preloadedForm && (
             <LiveForm formConfig={preloadedForm.formConfig} />
